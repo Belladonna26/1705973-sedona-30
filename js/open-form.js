de56712081a69b471fa1f84b14d52c8048a13559
Алена <alena.samsa@yandex.ru>
2021-03-19 12:48:25 +0400

    подключает факионки

---
+++ js/open-form.js
@@ -0,0 +1,8 @@
+let click = document.querySelector(".offer__search-button");
+let wrapper = document.querySelector(".offer__wrapper");
+
+
+click.addEventListener ("click", function() {
+wrapper.classList.toggle("offer_hidden");
+});
+
